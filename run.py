eed7eb2e210914b8b2f3f81e616cda12d636402c
httpcn <273528518@qq.com>
2021-05-07 21:16:10 +0800

    cors测试

--- run.py
+++ run.py
@@ -6,8 +6,14 @@ from auth import login_router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:9528",
+]
+
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
